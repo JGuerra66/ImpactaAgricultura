b1eff4988a4384916e9b2eb232e772d58baeb2ac
Juan Cruz Guerra <juancruzguerra66@gmail.com>
2024-01-07 04:42:22 -0300

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// next.config.js
+require('dotenv').config();
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // your next.js config here
+};
+
+module.exports = nextConfig;
